058e5480eb086dd43dc516bbd3868228b0f1a19a
hermani456 <hermani457@gmail.com>
2022-04-14 16:43:39 -0400

    update

--- Entendiendo a nuestros clientes/sequel.sql
+++ Entendiendo a nuestros clientes/sequel.sql
@@ -1,5 +1,5 @@
 -- export database
-pg_dump - U postgres unidad2 > C:/Users/diego/Desktop/unidad2.sql
+pg_dump -U postgres unidad2 > C:/Users/diego/Desktop/unidad2.sql
 
 CREATE DATABASE unidad2
 
@@ -12,21 +12,23 @@ psql -U postgres unidad2 < C:/Users/diego/Desktop/sql_db/unidad2.sql
 -- ● fecha: fecha del sistema.
 
 BEGIN TRANSACTION;
-INSERT INTO compra (cliente_id, fecha) VALUES (1, now());
-INSERT INTO detalle_compra (producto_id, compra_id, cantidad) VALUES (9, 39, 5);
+INSERT INTO compra (id, cliente_id, fecha) VALUES (33, 1, now());
 UPDATE producto SET stock = stock - 5 WHERE id = 9;
 COMMIT;
 
 -- El cliente usuario02 ha realizado la siguiente compra:
+-- ● producto: producto1, producto 2, producto 8.
+-- ● cantidad: 3 de cada producto.
+-- ● fecha: fecha del sistema.
 
 BEGIN TRANSACTION;
-INSERT INTO compra (cliente_id, fecha) VALUES (2, now());
-INSERT INTO detalle_compra (producto_id, compra_id, cantidad) VALUES (1, 41, 3);
+INSERT INTO compra (id, cliente_id, fecha) VALUES (34, 2, now());
+INSERT INTO detalle_compra (producto_id, compra_id, cantidad) VALUES (1, 34, 3);
 UPDATE producto SET stock = stock - 3 WHERE id = 1;
-INSERT INTO detalle_compra (producto_id, compra_id, cantidad) VALUES (2, 41, 3);
+INSERT INTO detalle_compra (producto_id, compra_id, cantidad) VALUES (2, 34, 3);
 UPDATE producto SET stock = stock - 3 WHERE id = 2;
 SAVEPOINT nueva_compra;
-INSERT INTO detalle_compra (producto_id, compra_id, cantidad) VALUES (8, 41, 3);
+INSERT INTO detalle_compra (producto_id, compra_id, cantidad) VALUES (8, 34, 3);
 UPDATE producto SET stock = stock - 3 WHERE id = 8;
 ROLLBACK TO nueva_compra;
 
