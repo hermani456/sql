0a1c786e4b585e49bb45c93292f33f2a3a5bbae8
hermani456 <hermani457@gmail.com>
2022-04-18 16:32:50 -0400

    fix

--- Aerolinea/sql.sql
+++ Aerolinea/sql.sql
@@ -24,8 +24,7 @@ CREATE TABLE vuelos (
    horario_despegue TIME NOT NULL,
    horario_aterrizaje TIME NOT NULL,
    asientos_disponibles SMALLINT NOT NULL,
-   precio INTEGER NOT NULL
-   CONSTRAINT asientos_disponibles_validos CHECK ((asientos_disponibles >= 0))
+   CONSTRAINT asientos_disponibles_validos CHECK ((asientos_disponibles > 0 AND asientos_disponibles <= 200))
 );
 
 CREATE TABLE pasajeros (
@@ -37,59 +36,48 @@ CREATE TABLE pasajeros (
 CREATE TABLE boletos (
    id SERIAL NOT NULL PRIMARY KEY,
    pasajeros_rut_fk VARCHAR(15) NOT NULL REFERENCES pasajeros(rut),
-   vuelos_fk INTEGER NOT NULL REFERENCES vuelos(id)
+   vuelos_fk INTEGER NOT NULL REFERENCES vuelos(id),
+   precio INTEGER NOT NULL
 );
 
 BEGIN TRANSACTION;
-INSERT INTO vuelos (nombre_ruta, horario_despegue, horario_aterrizaje, asientos_disponibles, precio)
-VALUES ('Ruta 1', '10:00', '12:00', 200, 100000);
-INSERT INTO vuelos (nombre_ruta, horario_despegue, horario_aterrizaje, asientos_disponibles, precio)
-VALUES ('Ruta 2', '10:00', '12:00', 200, 100000);
-INSERT INTO vuelos (nombre_ruta, horario_despegue, horario_aterrizaje, asientos_disponibles, precio)
-VALUES ('Ruta 3', '10:00', '12:00', 200, 100000);
-INSERT INTO vuelos (nombre_ruta, horario_despegue, horario_aterrizaje, asientos_disponibles, precio)
-VALUES ('Ruta 4', '10:00', '12:00', 200, 100000);
-INSERT INTO vuelos (nombre_ruta, horario_despegue, horario_aterrizaje, asientos_disponibles, precio)
-VALUES ('Ruta 5', '10:00', '12:00', 200, 100000);
+INSERT INTO vuelos (nombre_ruta, horario_despegue, horario_aterrizaje, asientos_disponibles)
+VALUES ('Ruta 1', '10:00', '12:00', 200);
+INSERT INTO vuelos (nombre_ruta, horario_despegue, horario_aterrizaje, asientos_disponibles)
+VALUES ('Ruta 2', '10:00', '12:00', 200);
+INSERT INTO vuelos (nombre_ruta, horario_despegue, horario_aterrizaje, asientos_disponibles)
+VALUES ('Ruta 3', '10:00', '12:00', 200);
+INSERT INTO vuelos (nombre_ruta, horario_despegue, horario_aterrizaje, asientos_disponibles)
+VALUES ('Ruta 4', '10:00', '12:00', 200);
+INSERT INTO vuelos (nombre_ruta, horario_despegue, horario_aterrizaje, asientos_disponibles)
+VALUES ('Ruta 5', '10:00', '12:00', 200);
 COMMIT;
 
--- INSERT INTO
---    pasajeros (rut, nombre, apellido)
--- VALUES
---    ('12345678-9', 'Juan', 'Perez'),
---    ('12345678-0', 'Pedro', 'Perez'),
---    ('12345678-1', 'Maria', 'Perez'),
---    ('12345678-2', 'Jose', 'Perez'),
---    ('12345678-3', 'Pepito', 'Perez');
-
+-- Agregar 5 pasajeros
 INSERT INTO pasajeros (rut, nombre, apellido)
 VALUES ('12345678-4', 'Juan', 'Perez');
-UPDATE vuelos SET asientos_disponibles = asientos_disponibles - 1 WHERE id = 1;
 INSERT INTO pasajeros (rut, nombre, apellido)
 VALUES('12345678-5', 'Pedro', 'Perez');
-UPDATE vuelos SET asientos_disponibles = asientos_disponibles - 1 WHERE id = 2;
 INSERT INTO pasajeros (rut, nombre, apellido)
 VALUES('12345678-6', 'Maria', 'Perez');
-UPDATE vuelos SET asientos_disponibles = asientos_disponibles - 1 WHERE id = 3;
 INSERT INTO pasajeros (rut, nombre, apellido)
 VALUES('12345678-7', 'Jose', 'Perez');
-UPDATE vuelos SET asientos_disponibles = asientos_disponibles - 1 WHERE id = 4;
 INSERT INTO pasajeros (rut, nombre, apellido)
 VALUES('12345678-8', 'Pepito', 'Perez');
-UPDATE vuelos SET asientos_disponibles = asientos_disponibles - 1 WHERE id = 5;
 
 -- Con transacción comprar un boleto (vuelo 1) a su nombre (pasajero 1)
 -- - luego cambiar de vuelo (vuelo 3)
 
 BEGIN TRANSACTION;
-INSERT INTO boletos (id, pasajeros_rut_fk, vuelos_fk) VALUES (1, '12345678-4', 1);
+INSERT INTO boletos (pasajeros_rut_fk, vuelos_fk, precio) VALUES ('12345678-4', 1, 100000);
 UPDATE vuelos SET asientos_disponibles = asientos_disponibles - 1 WHERE id = 1;
 UPDATE boletos SET vuelos_fk = 3 WHERE id = 1;
 UPDATE vuelos SET asientos_disponibles = asientos_disponibles + 1 WHERE id = 1;
 UPDATE vuelos SET asientos_disponibles = asientos_disponibles - 1 WHERE id = 3;
 COMMIT;
 
-
+-- mostrar que el vuelo inicial vuelva a quedar con la cantidad de asientos disponibles
+SELECT * FROM vuelos WHERE id = 1;
 
 
 
@@ -101,28 +89,32 @@ ALTER SEQUENCE boletos_id_seq RESTART WITH 1;
 truncate table vuelos cascade;
 
 
--- mostrar que el vuelo inicial vuelva a quedar con la cantidad de asientos disponibles
+
 
 
 -- comprar 5 boletos más y mostrar el total que ha ganado la aerolínea
 -- - mostrar que vuelo tiene más asientos disponibles
 -- - mostrar que vuelo tiene menos asientos disponibles
 
-INSERT INTO pasajeros (rut, nombre, apellido)
-VALUES ('12345678-9', 'Juan', 'Perez');
-UPDATE vuelos SET asientos_disponibles = asientos_disponibles - 1 WHERE id = 1;
-
 BEGIN TRANSACTION;
-INSERT INTO boletos (id, pasajeros_rut_fk, vuelos_fk) VALUES (2, '12345678-4', 1);
+INSERT INTO boletos (pasajeros_rut_fk, vuelos_fk, precio) VALUES ('12345678-4', 1, 100000);
 UPDATE vuelos SET asientos_disponibles = asientos_disponibles - 1 WHERE id = 1;
-INSERT INTO boletos (id, pasajeros_rut_fk, vuelos_fk) VALUES (3,'12345678-5', 2);
+INSERT INTO boletos (pasajeros_rut_fk, vuelos_fk, precio) VALUES ('12345678-5', 2, 100000);
 UPDATE vuelos SET asientos_disponibles = asientos_disponibles - 1 WHERE id = 2;
-INSERT INTO boletos (id, pasajeros_rut_fk, vuelos_fk) VALUES (4,'12345678-6', 3);
+INSERT INTO boletos (pasajeros_rut_fk, vuelos_fk, precio) VALUES ('12345678-6', 3, 100000);
 UPDATE vuelos SET asientos_disponibles = asientos_disponibles - 1 WHERE id = 3;
-INSERT INTO boletos (id, pasajeros_rut_fk, vuelos_fk) VALUES (5,'12345678-7', 4);
+INSERT INTO boletos (pasajeros_rut_fk, vuelos_fk, precio) VALUES ('12345678-7', 4, 100000);
 UPDATE vuelos SET asientos_disponibles = asientos_disponibles - 1 WHERE id = 4;
-INSERT INTO boletos (id, pasajeros_rut_fk, vuelos_fk) VALUES (6,'12345678-8', 4);
-UPDATE vuelos SET asientos_disponibles = asientos_disponibles - 1 WHERE id = 4;
+INSERT INTO boletos (pasajeros_rut_fk, vuelos_fk, precio) VALUES ('12345678-8', 5, 100000);
+UPDATE vuelos SET asientos_disponibles = asientos_disponibles - 1 WHERE id = 5;
 COMMIT;
 
+
+SELECT sum(boletos.precio) as ganancia_total
+FROM boletos 
+INNER JOIN vuelos ON boletos.vuelos_fk = vuelos.id;
+--Mostrar que vuelo tiene más asientos disponibles
+select * from vuelos where stock_asientos = (select max(stock_asientos) from vuelos) ;
+-- Mostrar que vuelo tiene menos asientos disponibles
+select * from vuelos where stock_asientos = (select min(stock_asientos) from vuelos) ;
 
